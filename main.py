369a59323e4bea30c651d048def7a089e5d82729
OlgaShopieva <113121816+OlgaShopieva@users.noreply.github.com>
2022-12-19 21:55:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5922438767:AAEtJtA9CYNFVWIpvyOINctX_mSvE1D5hXU')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+# @bot.message_handler(content_types=['text'])
+# def get_user_text(message):
+#     if message.text == "Hello":
+#         bot.send_message(message.chat.id, "И тебе привет!", parse_mode='html')
+#     elif message.text == "id":
+#         bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
+#     elif message.text == "photo":
+#         photo = open('1232312.png', 'rb')
+#         bot.send_photo(message.chat.id, photo)
+#     else:
+#         bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
+#
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, "Вау, крутое фото!")
+
+#
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Посетить веб сайт", url="https://kezuki.ru/"))
+    bot.send_message(message.chat.id, "Перейдите на сайт", reply_markup=markup)
+
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    website = types.KeyboardButton('Веб сайт')
+    start = types.KeyboardButton('Старт')
+
+    markup.add(website, start)
+    bot.send_message(message.chat.id, "Перейдите на сайт", reply_markup=markup)
+
+bot.polling(none_stop=True)
+
+
